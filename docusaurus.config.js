90cb9c540f0f8f8280eb2f384bf7bb066db4c215
Tai <4382091+taisukemino@users.noreply.github.com>
2022-06-08 17:33:13 +0900

    remove some things for the launch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,17 @@ const config = {
       "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/carapace-finance/website",
-        },
-        blog: {
-          showReadingTime: true,
-          editUrl: "https://github.com/carapace-finance/website",
-        },
+        // docs: {
+        //   routeBasePath: "/",
+        //   sidebarPath: require.resolve("./sidebars.js"),
+        //   editUrl: "https://github.com/carapace-finance/website",
+        // },
+        docs: false,
+        // blog: {
+        //   showReadingTime: true,
+        //   editUrl: "https://github.com/carapace-finance/website",
+        // },
+        blog: false,
         theme: {
           customCss: require.resolve("./src/style/custom.css"),
         },
@@ -47,22 +49,22 @@ const config = {
         },
         items: [
           { to: "/whitepaper", label: "White Paper", position: "right" },
-          {
-            type: "doc",
-            docId: "docs/abstract",
-            position: "right",
-            label: "Docs",
-            to: "/docs/docs/abstract",
-          },
-          {
-            type: "doc",
-            docId: "developers/intro",
-            position: "right",
-            label: "Developers",
-            to: "/docs/developers/intro",
-          },
+          // {
+          //   type: "doc",
+          //   docId: "docs/abstract",
+          //   position: "right",
+          //   label: "Docs",
+          //   to: "/docs/docs/abstract",
+          // },
+          // {
+          //   type: "doc",
+          //   docId: "developers/intro",
+          //   position: "right",
+          //   label: "Developers",
+          //   to: "/docs/developers/intro",
+          // },
           { to: "/jobs", label: "Jobs", position: "right" },
-          { to: "/blog", label: "Blog", position: "right" },
+          // { to: "/blog", label: "Blog", position: "right" },
         ],
       },
       footer: {
@@ -75,23 +77,23 @@ const config = {
                 label: "White Paper",
                 to: "/whitepaper",
               },
-              {
-                label: "Docs",
-                to: "/docs/abstract",
-              },
-              {
-                label: "Developers",
-                to: "/developers/intro",
-              },
+              // {
+              //   label: "Docs",
+              //   to: "/docs/abstract",
+              // },
+              // {
+              //   label: "Developers",
+              //   to: "/developers/intro",
+              // },
             ],
           },
           {
             title: "Community",
             items: [
-              {
-                label: "Discord",
-                href: "https://discord.gg/ZqeB5MHzjX",
-              },
+              // {
+              //   label: "Discord",
+              //   href: "https://discord.gg/ZqeB5MHzjX",
+              // },
               {
                 label: "Twitter",
                 href: "https://twitter.com/carapacefinance",
@@ -101,10 +103,10 @@ const config = {
           {
             title: "More",
             items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
+              // {
+              //   label: "Blog",
+              //   to: "/blog",
+              // },
               {
                 label: "GitHub",
                 href: "https://github.com/carapace-finance",
