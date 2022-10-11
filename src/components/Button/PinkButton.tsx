bddccf8a4a9879da4eec2e51121df7d478da3710
Ryota <ryoenoki2013@gmail.com>
2022-10-11 17:52:27 +0900

    pink button

--- src/components/Button/PinkButton.tsx
+++ src/components/Button/PinkButton.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 
 export const PinkButton = (props) => (
-  // <Link className={button.container} href={props.href} target={props.target}>
-  //   {props.text}
-  // </Link>
+  //! TODO: Change font
   <a href={props.href} target="_blank">
-    <button className={`bg-buttonPink text-white text-transform: uppercase ${props.fontSize} ${props.borderRadius} font-normal leading-[50px] px-12 py-7 text-center hover:bg-buttonPinkHover`}>{props.text}</button>
+    <button className={`bg-buttonPink text-white text-transform: uppercase font-normal leading-[50px] text-center hover:bg-buttonPinkHover ${props.paddingX} ${props.paddingY} ${props.fontSize} ${props.borderRadius} `}>{props.text}</button>
   </a>
 );
