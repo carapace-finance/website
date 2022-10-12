62d3e4751055e976d5d7a58d968dcc3eb730b882
Ryota <ryoenoki2013@gmail.com>
2022-10-12 19:29:07 +0900

    three layer description responsive

--- src/components/ContentMain.tsx
+++ src/components/ContentMain.tsx
@@ -7,13 +7,13 @@ const ContentMain = () => {
   return (
     <section id='content'>
       <div className="flex flex-col justify-center items-center">
-        <div className='flex flex-row flex-wrap'>
+        <div className='flex flex-row flex-wrap justify-center'>
           <AnimationWithTopText text="protect" bgImage="bg-[url('.././assets/animation1.png')]" />
           <div className='sm:w-[54px]'></div>
           <ThreeLayerDescription height="h-[410px]" width="w-[529px]" firstLayer="Earn risk-adjusted yields with protected loans." secondLayer="To earn risk-adjusted yields, you can lend money in under- collateralized loans protocols with protection." thirdLayer="Protected Loans"/>
         </div>
       <div className='h-[54px]'></div>
-      <div className='flex flex-row flex-wrap-reverse'>
+      <div className='flex flex-row flex-wrap-reverse md:flex-wrap justify-center'>
           <ThreeLayerDescription height="h-[430px]" width="w-[526px]" firstLayer="Earn yields by providing capital for credit default risks." secondLayer="You can earn yields by providing capital for default risks in under-collateralized loans." thirdLayer="Providing Capital for Sellers"/>
           <div className='sm:w-[54px]'></div>
           <AnimationWithTopText text="earn" bgImage="bg-[url('.././assets/animation2.png')]" />
