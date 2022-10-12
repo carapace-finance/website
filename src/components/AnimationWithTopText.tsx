a86c7070705dfb0c912723fa67946e2d2c5e053a
Ryota <ryoenoki2013@gmail.com>
2022-10-12 16:34:55 +0900

    animation with top text

--- src/components/AnimationWithTopText.tsx
+++ src/components/AnimationWithTopText.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const AnimationWithTopText = () => {
+
+const AnimationWithTopText = (props) => {
   return (
-    <div>AnimationWithTopText</div>
+    <div className={`${props.bgImage} bg-no-repeat bg-contain bg-center w-[290px] sm:w-[570px] h-[413px] sm:h-[615px]`}>
+      {/* Change font */}
+      <p className='font-semibold text-[54px] sm:text-[70px] leading-[50px] text-center tracking-[-0.06em] m-0'>
+        {props.text}
+      </p>
+    </div>
   )
 }
 
--- src/components/ContentMain.tsx
+++ src/components/ContentMain.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import AnimationWithTopText from './AnimationWithTopText'
 import ThreeLayerDescription from './ThreeLayerDescription'
 
+
 const ContentMain = () => {
   return (
     <section id='content'>
       <div className="flex flex-col justify-center items-center">
         <div className='flex flex-row'>
-          <AnimationWithTopText />
+          <AnimationWithTopText text="protect" bgImage="bg-[url('.././assets/animation1.png')]" />
+          <ThreeLayerDescription />
+        </div>
+                <div className='flex flex-row'>
+          <AnimationWithTopText text="protect" bgImage="bg-[url('.././assets/animation1.png')]" />
           <ThreeLayerDescription />
         </div>
       </div>
