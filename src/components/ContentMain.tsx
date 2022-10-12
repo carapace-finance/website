4149586bb55605343455e490459c325a6061ee67
Ryota <ryoenoki2013@gmail.com>
2022-10-12 17:57:55 +0900

    three layer description desktop

--- src/components/ContentMain.tsx
+++ src/components/ContentMain.tsx
@@ -9,10 +9,13 @@ const ContentMain = () => {
       <div className="flex flex-col justify-center items-center">
         <div className='flex flex-row'>
           <AnimationWithTopText text="protect" bgImage="bg-[url('.././assets/animation1.png')]" />
-          <ThreeLayerDescription firstLayer="Earn risk-adjusted yields with protected loans." secondLayer="To earn risk-adjusted yields, you can lend money in under- collateralized loans protocols with protection." thirdLayer="Protected Loans"/>
+          <div className='w-[54px]'></div>
+          <ThreeLayerDescription height="h-[410px]" width="w-[529px]" firstLayer="Earn risk-adjusted yields with protected loans." secondLayer="To earn risk-adjusted yields, you can lend money in under- collateralized loans protocols with protection." thirdLayer="Protected Loans"/>
         </div>
-                <div className='flex flex-row'>
-          <ThreeLayerDescription />
+      <div className='h-[54px]'></div>
+      <div className='flex flex-row'>
+          <ThreeLayerDescription height="h-[430px]" width="w-[526px]" firstLayer="Earn yields by providing capital for credit default risks." secondLayer="You can earn yields by providing capital for default risks in under-collateralized loans." thirdLayer="Providing Capital for Sellers"/>
+          <div className='w-[54px]'></div>
           <AnimationWithTopText text="earn" bgImage="bg-[url('.././assets/animation2.png')]" />
         </div>
       </div>
