c9027ffafbcb9984ac4611145ad9b2f746e59c8d
Ryota <ryoenoki2013@gmail.com>
2022-10-12 15:32:58 +0900

    hero responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,13 +11,13 @@ const Hero = () => {
             <br className='lg:hidden'/>
             <span className='tracking-[-0.02em] font-light text-7xl md:text-[150px]'>Defi Risk</span>
            </h1>
-          <p className='font-semibold text-white text-[38px] leading-[44px] text-center tracking-[-0.02em] max-w-[750px] font-manrope mt-0 mb-[59px]'>Invest in DeFi with protection against credit default risks.</p>
+          <p className='font-semibold text-white text-[30px] sm:text-[38px] leading-[34px] sm:leading-[44px] text-center tracking-[-0.02em] max-w-[300px] sm:max-w-[750px] font-manrope mt-0 mb-[79px] sm:mb-[59px]'>Invest in DeFi with protection against credit default risks.</p>
           <PinkButton
-          fontSize="text-3xl"
+          fontSize="sm:text-3xl text-lg"
           borderRadius="rounded-[200px]"
           text="Sign-up for our Beta App"
-          paddingX="px-12"
-          paddingY="py-7"
+          paddingX="px-7 sm:px-12"
+          paddingY="py-7 sm:py-12"
         />
         </div>
       </div>
