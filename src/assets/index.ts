4ff00d406894c1e497a2a8707151b46db89d2e9f
Ryota <ryoenoki2013@gmail.com>
2022-10-12 15:54:32 +0900

    main content structure

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,9 @@
 import hero from './hero.png';
+import animation1 from './animation1.png';
+;import animation2 from './animation2.png';
 
 export default {
   hero,
+  animation1,
+  animation2,
 };
---
+++ src/components/ContentMain.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContentMain = () => {
+  return (
+    <section id='content'>
+
+    </section>
+  )
+}
+
+export default ContentMain
