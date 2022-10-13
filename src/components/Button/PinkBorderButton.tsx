d829eb5e90a624cc3356d660fbdf6e25a030303a
Ryota <ryoenoki2013@gmail.com>
2022-10-13 18:17:30 +0900

    lending protocol wip

--- src/components/Button/PinkBorderButton.tsx
+++ src/components/Button/PinkBorderButton.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
 
-const PinkBorderButton = () => {
-  return (
-    <div>PinkBorderButton</div>
-  )
-}
-
-export default PinkBorderButton
+export const PinkBorderButton = (props) => (
+  //! TODO: Change font
+  <a href={props.href} target="_blank">
+    <button className={`border-4 border-buttonPink rounded-[200px] text-white tracking-[0.02em] text-transform: uppercase font-normal leading-[50px] text-center hover:text-buttonPinkHover hover:border-buttonPinkHover transition-all px-[65px] sm:px-[55px] py-[10px] sm:py-[27px] text-lg sm:text-3xl`}>{props.text}</button>
+  </a>
+);
--- src/components/LendingProtocol.tsx
+++ src/components/LendingProtocol.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import assets from '../assets'
+import { PinkBorderButton } from './Button/PinkBorderButton'
 
 const LendingProtocol = () => {
   return (
     <section id='lendingProtocol'>
-      <div className="h-screen w-screen bg-customDarkBlue flex flex-col items-center justify-center">
+      <div className="h-full w-screen bg-customDarkBlue flex flex-col items-center justify-center">
+        <div className='h-[73px] sm:h-[121px]'></div>
         <img className='w-[189px] sm:w-[215px] h-[85px] sm:h-[107px]' src={assets.eyeOpen} alt="open eye" />
         <div className='h-[55px] sm:h-[65px]'></div>
         <p className='font-manrope font-extrabold text-[42px] sm:text-[120px] leading-[50px] sm:leading-[125px] text-center tracking-tighter text-white m-0'>Lending Protocol?</p>
         <div className='h-[28px] sm:h-[36px]'></div>
         <p className='sm:max-w-full max-w-[348px] font-manrope font-medium text-[22px] sm:text-[32px] leading-[30px] sm:leading-[44px] text-center tracking-[0.01em] text-white m-0'>You are running a credit protocol and you want to work with us ? </p>
-        <div className='h-[47px] sm:h-[67px]'></div>
-
+        <div className='h-[65px] sm:h-[67px]'></div>
+        <PinkBorderButton text="Let's chat" />
+        <div className='h-[91px] sm:h-[151px]'></div>
       </div>
     </section>
   )
