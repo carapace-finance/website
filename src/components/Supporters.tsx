c15c507ce5be484384e64c6c60a4f6c58cf53473
Ryota <ryoenoki2013@gmail.com>
2022-10-13 12:04:46 +0900

    supporters wip

--- src/components/Supporters.tsx
+++ src/components/Supporters.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { supportingCompanies } from '.././constants'
+import WhiteCard from './WhiteCard'
 
 const Supporters = () => {
   return (
@@ -6,7 +8,14 @@ const Supporters = () => {
       <div className='flex flex-col justify-center items-center'>
         <p className='font-extrabold font-manrope text-4xl sm:text-[64px] leading-[18px] text-center tracking-tighter text-customDarkBlue opacity-30 mt-0 mb-7'>Our Supporters</p>
         <p className='font-normal font-manrope text-[22px] sm:text-[32px] leading-[44px] text-center tracking-[0.01em] text-customDarkBlue opacity-30'>Carapace is supported by top entrepreneurs and investors</p>
-        <div className='h-[75px]'></div>
+        <div className='h-[54px] sm:h-[75px]'></div>
+        <div className='grid grid-cols-2 lg:grid-cols-3 grid-rows-3 lg:grid-rows-2 gap-x-[24px] lg:gap-x-[29.57px] gap-y-[23px] lg:gap-y-[37.2px]'>
+          {supportingCompanies.map((company, _) => (
+            <div key={company.id}>
+              <WhiteCard company={company.title} />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
---
+++ src/constants/index.ts
@@ -0,0 +1,26 @@
+export const supportingCompanies = [
+  {
+    id: "supportingCompanies-1",
+    title: "NFX",
+  },
+  {
+    id: "supportingCompanies-2",
+    title: "TRIBE CAPITAL",
+  },
+  {
+    id: "supportingCompanies-3",
+    title: "Ledger Prime",
+  },
+  {
+    id: "supportingCompanies-4",
+    title: "GSR Ventures",
+  },
+  {
+    id: "supportingCompanies-5",
+    title: "Titan Capital",
+  },
+  {
+    id: "supportingCompanies-6",
+    title: "SYNTHETIX",
+  },
+];
