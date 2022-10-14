8c86c52fe460f0be376fb99b78941c39a791edaa
Ryota <ryoenoki2013@gmail.com>
2022-10-14 16:27:10 +0900

    responsive

--- src/components/CircleAvatarWithName.tsx
+++ src/components/CircleAvatarWithName.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CircleAvatarWithName = (props) => {
   return (
-    <a href={props.link} target="_blank" className='hover:no-underline mt-[50px]'>
+    <a href={props.link} target="_blank" className='hover:no-underline mt-[20px] lg:mt-[50px]'>
     <div className='flex flex-col items-center justify-center text-opacity-30 opacity-50 hover:opacity-100' key={props.id} >
       <img className='grayscale rounded-full h-[60px] sm:h-[68px] w-[60px] sm:w-[68px] object-cover object-center' src={props.profile} alt="supporter" />
       <div className='sm:h-[33px] h-4'></div>
