42d6428a5c959defd2de4ac8ba053e64b872218b
Tai <4382091+taisukemino@users.noreply.github.com>
2022-10-18 13:27:56 +0900

    style the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,15 +9,15 @@ const Hero = () => {
         <div className="flex flex-col justify-center items-center h-screen">
            <h1 className="tracking-tighter font-extrabold text-white text-9xl md:text-[180px] font-manrope">DeFy{" "}
             <br className='lg:hidden'/>
-            <span className='tracking-[-0.02em] font-light text-7xl md:text-[150px]'>Defi Risk</span>
+            <span className='tracking-[-0.02em] font-light text-7xl md:text-[150px]'>DeFi Risk</span>
            </h1>
-          <p className='font-semibold text-white text-[30px] sm:text-[38px] leading-[34px] sm:leading-[44px] text-center tracking-[-0.02em] max-w-[300px] sm:max-w-[750px] font-manrope mt-0 mb-[79px] sm:mb-[59px]'>Invest in DeFi with protection against credit default risks.</p>
+          <p className='font-semibold text-white text-[30px] sm:text-[38px] leading-[34px] sm:leading-[44px] text-center tracking-[-0.02em] max-w-[300px] sm:max-w-[750px] font-manrope mt-16 mb-[79px] sm:mb-[59px]'>Invest in DeFi with protection against credit default risks.</p>
           <PinkButton
           fontSize="sm:text-[25.5px] text-lg"
           borderRadius="rounded-[200px]"
-          text="Sign-up for our Beta App"
-          paddingX="px-7 sm:px-9"
-          paddingY="py-7 sm:py-9"
+          text="Sign-up for our Beta"
+          paddingX="px-8 sm:px-8"
+          paddingY="py-4 sm:py-4"
           href="https://xzpl2jauxb6.typeform.com/to/hSmuyZph"
         />
         </div>
