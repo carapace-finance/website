e5dc21a497c896406a23091449bab80767b170f4
Ryota <ryoenoki2013@gmail.com>
2022-10-21 09:42:32 +0900

    remove comment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-// require('dotenv').config()
+require('dotenv').config()
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -16,7 +16,7 @@ const config = {
   favicon: "img/favicon.ico",
   organizationName: "Carapace",
   projectName: "website",
-  // plugins: [require.resolve("docusaurus-plugin-fathom")],
+  plugins: [require.resolve("docusaurus-plugin-fathom")],
   presets: [
     [
       "classic",
