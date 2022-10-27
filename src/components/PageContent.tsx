2fedd0e642c97e2527ebd1cf78e3b35cb07b7b11
Ryota <ryoenoki2013@gmail.com>
2022-10-27 19:27:49 +0900

    whitepaper wip

---
+++ src/components/PageContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageContent = () => {
+  return (
+    <div className='bg-buttonPink col-span-7 pt-[100px]'>
+      <p className=''>Carapace Protocol</p>
+    </div>
+  )
+}
+
+export default PageContent
---
+++ src/components/PageSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageSidebar = () => {
+  return (
+    <div className='bg-black border-r col-span-3'>PageSidebar</div>
+  )
+}
+
+export default PageSidebar
