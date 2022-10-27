ceb570a6e26ad9cec4e1e220a0ad14925a8203a4
Ryota <ryoenoki2013@gmail.com>
2022-10-28 06:52:06 +0900

    white paper

--- src/components/PageSidebar.tsx
+++ src/components/PageSidebar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PageSidebar = () => {
   return (
-    <div className='bg-black border-r col-span-3'>PageSidebar</div>
+    <div className='bg-black border-r col-span-2'>PageSidebar</div>
   )
 }
 
