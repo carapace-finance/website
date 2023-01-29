816615c35c3fd1137f3c9db36466861997c3648d
Asif <asifkhan0410@gmail.com>
2023-01-29 16:34:38 +0530

    hero text changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,11 +8,11 @@ const Hero = () => {
     <section id='home'>
       <div className="h-screen w-screen bg-[url('.././assets/heroBanner.gif')] bg-no-repeat bg-cover bg-center">
         <div className="flex flex-col justify-center items-center h-screen">
-           <h1 className="tracking-tighter font-extrabold text-white text-9xl md:text-[180px] font-manrope animate-slide-in-bottom">DeFy{" "}
+           <h1 className="tracking-tighter font-extrabold text-white text-9xl md:text-[180px] font-manrope animate-slide-in-bottom text-center">Earn{" "}
             <br className='lg:hidden'/>
-            <span className='tracking-[-0.02em] font-light text-7xl md:text-[150px] animate-slide-in-bottom'>DeFi Risk</span>
+            <span className='tracking-[-0.02em] font-light text-7xl md:text-[150px] animate-slide-in-bottom'>To Protect</span>
            </h1>
-          <p className='font-semibold text-white text-[30px] sm:text-[38px] leading-[34px] sm:leading-[44px] text-center tracking-[-0.02em] max-w-[300px] sm:max-w-[750px] font-manrope mt-16 mb-[79px] sm:mb-[59px] animate-text-focus-in'>Invest in DeFi with protection against credit default risk.</p>
+          <p className='font-semibold text-white text-[30px] sm:text-[38px] leading-[34px] sm:leading-[44px] text-center tracking-[-0.02em] max-w-[300px] sm:max-w-[750px] font-manrope mt-16 mb-[79px] sm:mb-[59px] animate-text-focus-in'>Experience the future of credit investing with on-chain protection markets.</p>
           {/* Delete if uncecessary */}
           {/* <PinkButton
           fontSize="sm:text-[25.5px] text-lg"
