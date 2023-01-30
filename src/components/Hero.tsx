8be0b032dfe20dd82d059bf91e61e94a106585b3
Tai <4382091+taisukemino@users.noreply.github.com>
2023-01-30 21:08:58 +0900

    use the lower case in the tag line

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <div className="flex flex-col justify-center items-center h-screen">
            <h1 className="tracking-tighter font-extrabold text-white text-9xl md:text-[180px] font-manrope animate-slide-in-bottom text-center">Earn{" "}
             <br className='lg:hidden'/>
-            <span className='tracking-[-0.02em] font-light text-7xl md:text-[150px] animate-slide-in-bottom'>To Protect</span>
+            <span className='tracking-[-0.02em] font-light text-7xl md:text-[150px] animate-slide-in-bottom'>to Protect</span>
            </h1>
           <p className='font-semibold text-white text-[30px] sm:text-[38px] leading-[34px] sm:leading-[44px] text-center tracking-[-0.02em] max-w-[300px] sm:max-w-[750px] font-manrope mt-16 mb-[79px] sm:mb-[59px] animate-text-focus-in'>Experience the future of credit investing with on-chain protection markets.</p>
           {/* Delete if uncecessary */}
