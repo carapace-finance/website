efbd863d01acb6b4df9c4a8272e9c3081ce73ca7
Asif <asifkhan0410@gmail.com>
2023-02-07 02:54:57 +0530

    add the viral-loop signup

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,5 @@
+import Link from '@docusaurus/Link'
 import React from 'react'
-import { PinkBorderButton } from './Button/PinkBorderButton'
-import { PinkButton } from './Button/PinkButton'
-
 
 const Hero = () => {
   return (
@@ -13,22 +11,9 @@ const Hero = () => {
             <span className='tracking-[-0.02em] font-light text-7xl md:text-[150px] animate-slide-in-bottom'>to Protect</span>
            </h1>
           <p className='font-semibold text-white text-[30px] sm:text-[38px] leading-[34px] sm:leading-[44px] text-center tracking-[-0.02em] max-w-[300px] sm:max-w-[750px] font-manrope mt-16 mb-[79px] sm:mb-[59px] animate-text-focus-in'>Experience the future of credit investing with on-chain protection markets.</p>
-          {/* Delete if uncecessary */}
-          {/* <PinkButton
-          fontSize="sm:text-[25.5px] text-lg"
-          borderRadius="rounded-[200px]"
-          text="Sign-up for Beta"
-          paddingX="px-8 sm:px-8"
-          paddingY="py-4 sm:py-4"
-          href="https://xzpl2jauxb6.typeform.com/to/hSmuyZph"
-        /> */}
-        <PinkBorderButton
-          fontSize="sm:text-[25.5px] text-lg"
-          text="Sign-up for Beta"
-          paddingX="px-8 sm:px-8"
-          paddingY="py-4 sm:py-4"
-          href="https://xzpl2jauxb6.typeform.com/to/hSmuyZph"
-        />
+        <Link href='/join-the-waitlist'>
+          <button className={`border-4 border-buttonPink rounded-[200px] text-white tracking-[0.02em] text-transform: uppercase font-normal leading-[50px] text-center hover:bg-buttonPink hover:border-buttonPink ease-in-out duration-300 transition-all px-8 sm:px-8 py-4 sm:py-4 sm:text-[25.5px] text-lg`}>{'Sign-up for Beta'}</button>
+        </Link>
         </div>
       </div>
     </section>
---
+++ src/pages/join-the-waitlist.tsx
@@ -0,0 +1,15 @@
+import Head from '@docusaurus/Head'
+import React from 'react'
+
+const JoinTheWaitlist = () => {
+    return (
+        <div className='min-h-[50vh] flex item-center justify-center'>
+            <Head>
+                <script type="text/javascript" src="https://app.viral-loops.com/widgetsV2/core/loader.js" ></script>
+            </Head>
+            <form-widget ucid='S0kxDt6zg93b9IA0Xs2XoOhh9oU'></form-widget>
+        </div>
+    )
+}
+
+export default JoinTheWaitlist
